0285a387dd36b2bb85a403cabfef0bb16c10875c
LenaFedchenko <lenafedcenko98@gmail.com>
2025-08-29 22:44:55 +0300

    fixed bags

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -10,7 +10,6 @@ namespace CVHub.Data
             : base(options)
         {
         }
-        public DbSet<Registration> Registrations { get; set; }
         public DbSet<Registration> Users { get; set; } = null!;
         public DbSet<Account> Accounts { get; set; } = null!;
 
--- obj/Debug/net9.0/CVHub.AssemblyInfo.cs
+++ obj/Debug/net9.0/CVHub.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CVHub")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f0fde33da9ce9565961f8fde0018e6b3d626b40")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1f7d4d8f902a630d8292757dcbb495979bfc2d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CVHub")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CVHub")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
